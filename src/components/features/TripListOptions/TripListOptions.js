41ccb8a55060bceed96bdd31ab398c9fd86cea7e
Tomasz Jarosiewicz <tomasz@jarosiewicz.pl>
2020-09-29 20:05:11 +0200

    Add dispatchers

--- src/components/features/TripListOptions/TripListOptions.js
+++ src/components/features/TripListOptions/TripListOptions.js
@@ -9,15 +9,18 @@ class TripListOptions extends React.Component {
     if(checked) {
       console.log('Adding tag', tag);
       // TODO - use action dispatcher from props
+      this.props.searchTags(tag);
     } else {
       console.log('Removing tag', tag);
       // TODO - use action dispatcher from props
+      this.props.searchTags(tag);
     }
   }
 
   handleDuration(type, value){
     console.log('Changing duration', type, value);
     // TODO - use action dispatcher from props
+    this.props.searchDuration(value);
   }
 
   handleSearch(phrase){
@@ -73,6 +76,8 @@ TripListOptions.propTypes = {
   tags: PropTypes.object,
   filters: PropTypes.object,
   changeSearchPhrase: PropTypes.func,
+  searchTags: PropTypes.func,
+  searchDuration: PropTypes.func,
 };
 
 export default TripListOptions;

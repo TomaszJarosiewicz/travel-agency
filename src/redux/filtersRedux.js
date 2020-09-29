42b240d4f99dcfb5ee6cbca84803558e0d3ee7cd
Tomasz Jarosiewicz <tomasz@jarosiewicz.pl>
2020-09-29 20:03:23 +0200

    Add types, creators, reducer and filters

--- src/redux/filtersRedux.js
+++ src/redux/filtersRedux.js
@@ -11,10 +11,13 @@ const createActionName = name => `app/${reducerName}/${name}`;
 // action types
 export const CHANGE_PHRASE = createActionName('CHANGE_PHRASE');
 // TODO - add other action types
-
+export const CHANGE_TAGS = createActionName('CHANGE_TAGS');
+export const CHANGE_DURATION = createActionName('CHANGE_DURATION');
 // action creators
 export const changeSearchPhrase = payload => ({ payload, type: CHANGE_PHRASE });
 // TODO - add other action creators
+export const searchTags = payload => ({ payload, type: CHANGE_TAGS });
+export const searchDuration = payload => ({ payload, type: CHANGE_DURATION });
 
 // reducer
 export default function reducer(statePart = [], action = {}) {
@@ -25,6 +28,16 @@ export default function reducer(statePart = [], action = {}) {
         searchPhrase: action.payload,
       };
     // TODO - handle other action types
+    case CHANGE_TAGS:
+      return {
+        ...statePart,
+        tags: action.payload,
+      };
+    case CHANGE_DURATION:
+      return {
+        ...statePart,
+        duration: action.payload,
+      };
     default:
       return statePart;
   }
--- src/redux/tripsRedux.js
+++ src/redux/tripsRedux.js
@@ -40,22 +40,19 @@ export const getTripsForCountry = ({trips}, countryCode) => {
 
 /* ACTIONS */
 
-/*
 // action name creator
 const reducerName = 'trips';
 const createActionName = name => `app/${reducerName}/${name}`;
 
 // action types
 
-
 // action creators
 
-
-// reducer
-export default function reducer(statePart = [], action = {}) {
-  switch (action.type) {
-    default:
-      return statePart;
+reducer
+  export default function reducer(statePart = [], action = {}) {
+    switch (action.type) {
+      default:
+        return statePart;
+    }
   }
-}
- */
+

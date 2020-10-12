7b99a9bda9e17522440a8743ee9fcbd59e569f20
Tomasz Jarosiewicz <tomasz@jarosiewicz.pl>
2020-10-12 21:41:05 +0200

    Add datapicker

--- src/components/features/OrderOption/OrderOptionDate.js
+++ src/components/features/OrderOption/OrderOptionDate.js
@@ -3,7 +3,6 @@ import DatePicker from "react-datepicker";
 import styles from './OrderOption.scss';
 import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 
-
 const OrderOptionDate = () => {
   const [startDate, setStartDate] = useState(new Date());
   return (
